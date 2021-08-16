7d7fb4043afdaa0a56bb8a35e8d82a3e4c732dcf
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2021-08-16 18:32:28 +0530

    Quiz.com added!

---
+++ Quiz.com/db/db.js
@@ -0,0 +1,12 @@
+const db = [
+    {
+        question: "Hello World",
+        a: "Hello wOrld",
+        b: "bye world",
+        c: "say world",
+        d: "gadha world",
+        ans: "Hello world"
+    }
+]
+
+module.exports = db;
---
+++ Quiz.com/static/Js/home.js
@@ -0,0 +1,12 @@
+// import { db } from "../../db/db"
+// const db = require("../../db/db")
+console.log("Hello world")
+
+
+// const fetchData = async () => {
+//     const response = await fetch("../../db/db.js")
+//     const data = await response.json();
+//     console.log(data)
+// }
+
+// fetchData()
