24a221f1fb149bb81c9cf3434664cbad0de97b96
Aryan Sisodiya <aryansisodiya091107@gmail.com>
2022-10-01 16:44:38 +0530

    some changes done

--- Coding-Website Project/static/Js/home.js
+++
@@ -1,9 +0,0 @@
-const header = document.querySelector('.header');
-const hamburger = document.getElementById("hamburger");
-const hideContent = document.querySelector(".hide-content");
-
-hamburger.addEventListener('click', () => {
-    hideContent.classList.toggle('show-nav-resp');
-    header.classList.toggle("h-header-resp")
-});
-
